f0e984b697afe1fac15c6cb927c7b84ffa4a89d2
Ilnur Arifullin <ChloePrice34@yandex.ru>
2022-01-06 22:08:39 +0300

    Исправления после ревью

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -14,8 +14,6 @@ class Group(models.Model):
 
 
 class Post(models.Model):
-    class Meta:
-        ordering = ('-pub_date',)
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
@@ -31,6 +29,9 @@ class Post(models.Model):
         blank=True,
         related_name='posts',
     )
+
+    class Meta:
+        ordering = ('-pub_date',)
 
     def __str__(self):
         return self.text[15]
