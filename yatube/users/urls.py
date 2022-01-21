85ece2f8560a462027cf0e5903c97a2f03f4d517
Ilnur Arifullin <ChloePrice34@yandex.ru>
2022-01-21 05:16:13 +0300

    UPD по работам с паролями пользователей

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeDoneView, PasswordChangeView, PasswordResetView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeDoneView, PasswordChangeView, PasswordResetCompleteView, PasswordResetConfirmView, PasswordResetDoneView, PasswordResetView
 from django.urls import path
 from . import views
 
@@ -11,4 +11,7 @@ urlpatterns = [
     path('password_change/', PasswordChangeView.as_view(template_name='users/password_change.html'),name='password_change_form'),
     path('password_change/done/', PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),name='password_change_done'),
     path('password_reset/', PasswordResetView.as_view(template_name='users/password_reset_form.html'),name='password_reset'),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/' ,PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
+    path('password_reset/done/' ,PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
 ]
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -76,6 +76,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'yatube.wsgi.application'
 
+#  подключаем движок filebased.EmailBackend
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# указываем директорию, в которую будут складываться файлы писем
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails') 
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
